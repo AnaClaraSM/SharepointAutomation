5e5af805185ce4609591491887b264ea529fe4b8
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2025-04-03 09:27:57 -0300

    first commit - links and links' for

---
+++ inserirLinksNoticias/inserirLinkNoticia.py
@@ -0,0 +1,5 @@
+from links import links # Import links list from links.py file
+
+# For each link in the list of links
+for link in links:
+    print(f"Adicionando '{link}'...")
---
+++ inserirLinksNoticias/links.py
@@ -0,0 +1,7 @@
+# List of links(URLs)
+links = [
+    "https://www.terra.com.br/byte/nvidia-acabou-de-quebrar-a-equacao-que-previa-o-futuro-da-tecnologia-segundo-seu-ceo-a-ia-precisa-da-sua-propria-lei-de-moore,68a54bd6a989293cb0507cf4504806dc29sd2mtl.html",
+    "https://www.ibm.com/br-pt/think/topics/ai-chip",
+    "https://tecnoblog.net/noticias/copilot-microsoft-enfim-expande-ia-em-notebooks-com-chip-intel-ou-amd/",
+    "https://www.infomoney.com.br/business/openai-anuncia-rodada-de-us-40-bi-liderada-pelo-softbank-com-valuation-de-us-300-bi/"
+]
