29677d0b11469d2db0e6eec5798dac6fc0f0a474
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2025-04-03 11:55:26 -0300

    automation, delays added

--- inserirLinksNoticias/automacaoInserirLinkNoticia.py
+++ inserirLinksNoticias/automacaoInserirLinkNoticia.py
@@ -1,6 +1,22 @@
-import pyautogui
-from links import links # Import links list from links.py file
+import pyautogui # PyAutoGUI para automações RPA
+import time # Biblioteca de tempo
+from links import links # Importa a lista links do arquivo links.py
+from coordenadasAutomacao import * # Importa tudo do arquivo de coordenadas
 
-# For each link in the list of links
+pyautogui.PAUSE = 0.5 # Pausa de 0.5s entre cada ação do pyautogui
+
+# Para cada link da lista de links
 for link in links:
-    print(f"Adicionando '{link}'...")
+    pyautogui.moveTo(addNewsBtn["x"], addNewsBtn["y"])
+    pyautogui.click()
+    pyautogui.moveTo(addNewsLinkBtn["x"], addNewsLinkBtn["y"])
+    pyautogui.click()
+    pyautogui.moveTo(newsLinkField["x"], newsLinkField["y"])
+    pyautogui.click()
+    pyautogui.typewrite(link)
+    pyautogui.moveTo(postNewsLinkBtn["x"], postNewsLinkBtn["y"])
+    time.sleep(5)
+    pyautogui.click()
+    time.sleep(5)
+    print(f"Link '{link}' adicionado com sucesso!")
+print(f"{len(links)} links adicionados!")
