d5f71f24532b74643eaef1678d40665c9555b51a
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2025-04-11 11:56:09 -0300

    new playlist link to test

--- inserirVideosPlaylist/Selenium/variables.py
+++ inserirVideosPlaylist/Selenium/variables.py
@@ -9,7 +9,7 @@ office_home_url = "https://m365.cloud.microsoft/?auth=2"
 # YOUTUBE
 
 # Link da Playlist de Vídeos
-yt_playlist_url = "https://www.youtube.com/playlist?list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV"
+yt_playlist_url = "https://www.youtube.com/playlist?list=PLpdAy0tYrnKyjrY1Fr72DhmrRmeWI_5C8"
 
 # Seletor de Vídeo Individual da Playlist (Tag a)
 yt_playlist_video_selector = "a.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer"
@@ -21,7 +21,7 @@ yt_playlist_video_selector = "a.yt-simple-endpoint.style-scope.ytd-playlist-vide
 sp_page_url = "https://governosp.sharepoint.com/sites/SEADE-Gecon2/SitePages/LogicaProgramacaoCursoEmVideo.aspx"
 
 # Link da Página em Modo de Edição
-sp_page_edit_url = "https://governosp.sharepoint.com/sites/SEADE-Gecon2/SitePages/LogicaProgramacaoCursoEmVideo.aspx?Mode=Edit"
+sp_page_edit_url = "https://governosp.sharepoint.com/sites/SEADE-Gecon2/SitePages/Automacoes-com-Python.aspx?Mode=Edit"
 
 # Seletor da seção rolável da página
 sp_scrollable_section_selector = "[data-automation-id=\"contentScrollRegion\"]"
